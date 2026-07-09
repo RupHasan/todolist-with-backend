c2a27e7c16b5f3fbd7038841a666e5d3b5f125f4
Rup Hasan <hasanrup7@gmail.com>
2026-07-10 00:01:53 +0600

    very basic connection with the rest api server js

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 function sendData() {
     const taskName = document.getElementById("userInput").value;
     document.getElementById("userInput").value = '';
-    console.log(taskName);
     
-    // fetch('http://localhost:3000/todos',{
-    //     method: 'POST',
-    //     headers: {'Content-Type':'application/json'},
-    //     body: JSON.stringify({task: taskName})
-    // })
-    // .then(data => data.json())
-    // .then((data)=>{
-    //     console.log(data)
-    // })
-    
-    fetch('http://localhost:3000/todos')
-  .then(r => r.json())
-  .then(console.log)
+    fetch('http://localhost:3000/todos',{
+        method: 'POST',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({task: taskName})
+    })
+    .then(data => data.json())
+    .then((data)=>{
+    alert(data)
+    })
 }
+
+function render() {
+    fetch('http://localhost:3000/todos')
+    .then(data => data.json())
+    .then((data)=>{
+        data.forEach((item)=>{
+            document.getElementById("todo-container").innerHTML += `
+                <div id="todo-sub-container">
+                    <p>id: ${item.id}</p>
+                    <p>task: ${item.task}</p>
+                    <p>done status: ${item.done}</p>
+                </div>
+            `;
+        })
+    })
+}
+render()
