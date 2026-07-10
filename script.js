0d38b3a5fb04c9da4fe3c1eb15bd1bca803708db
Rup Hasan <hasanrup7@gmail.com>
2026-07-10 17:24:18 +0600

    works perfectly, though need to add css and some more button features

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
 function sendData() {
     const taskName = document.getElementById("userInput").value;
-    document.getElementById("userInput").value = '';
-    
-    fetch('http://localhost:3000/todos',{
-        method: 'POST',
-        headers: {'Content-Type':'application/json'},
-        body: JSON.stringify({task: taskName})
-    })
-    .then(data => data.json())
-    .then((data)=>{
-    alert(data)
-    })
+    document.getElementById("userInput").value = "";
+
+    if (taskName != "") {
+        fetch("http://localhost:3000/todos", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ task: taskName })
+        })
+            .then(data => data.json())
+        render();
+    } else {
+        alert("Enter task name first!");
+    }
 }
 
 function render() {
-    fetch('http://localhost:3000/todos')
-    .then(data => data.json())
-    .then((data)=>{
-        data.forEach((item)=>{
-            document.getElementById("todo-container").innerHTML += `
-                <div id="todo-sub-container">
-                    <p>id: ${item.id}</p>
+    document.getElementById("todo-container").innerHTML = "";
+    
+    fetch("http://localhost:3000/todos")
+        .then(data => data.json())
+        .then(data => {
+            data.forEach(item => {
+                document.getElementById("todo-container").innerHTML += `
+                <div class="todo-sub-container">
                     <p>task: ${item.task}</p>
                     <p>done status: ${item.done}</p>
+                    <button class="todo-del-btn" onclick="deleteTodo(${item.id})">Delete</button>
                 </div>
             `;
-        })
-    })
+            });
+        });
 }
-render()
+render();
+
+function deleteTodo(idNum) {
+    fetch(`http://localhost:3000/todos/${idNum}`, {
+        method: "DELETE"
+    });
+    
+    render();
+}
--- server.js
+++ server.js
@@ -1,84 +1,96 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-let todo = [];
+const fs = require("fs");
+let todo = JSON.parse(fs.readFileSync("todos.json", "utf8"));
 
 app.use(express.json());
-app.use(express.static('.'));
+app.use(express.static("."));
 
-app.get('/todos', (req,res)=>{
-    res.json(todo);
-})
+app.get("/todos", (req, res) => {
+    res.json(JSON.parse(fs.readFileSync("todos.json", "utf8")));
+});
 
-app.get('/', (req,res)=>{
-    res.json(todo);
-})
+app.get("/", (req, res) => {
+    res.json(JSON.parse(fs.readFileSync("todos.json", "utf8")));
+});
 
-app.get("/todos/:id",(req,res)=>{
+app.get("/todos/:id", (req, res) => {
     const wantedId = parseInt(req.params.id);
-    const findTodo = todo.find(td => td.id == wantedId)
-    
-    if(findTodo != undefined) {
+    const findTodo = todo.find(td => td.id == wantedId);
+
+    if (findTodo != undefined) {
         res.json(findTodo);
     } else {
-        res.status(404).send("Task not found in database")
+        res.status(404).send("Task not found in database");
     }
-})
+});
 
-
-app.post('/todos',(req,res)=>{
+app.post("/todos", (req, res) => {
     const task = req.body.task;
     todoLength = todo.length + 1;
     todo.push({
         id: todoLength,
         task: task,
         done: false
-    })
-    
-    res.json(todo[todoLength - 1]);
-})
+    });
 
-app.delete("/todos/:id",(req,res)=>{
+    fs.writeFileSync("todos.json", JSON.stringify(todo));
+    todo = JSON.parse(fs.readFileSync("todos.json", "utf8"));
+    res.json(todo[todoLength - 1]);
+});
+
+app.delete("/todos/:id", (req, res) => {
     const wantedId = parseInt(req.params.id);
     const todoToDel = todo.find(td => td.id == wantedId);
-    
-    if (todoToDel) {
-        for (let i = 0; i < todo.length; i++){
-            let checkTodo = todo[i]
-            if(checkTodo.id == wantedId) {
-                todo.splice(i, 1);
-            }
-        } 
-        
-        res.status(204).send("todo deleted!")
-    } else {
-        res.status(404).send("Todo Not Found To Delete! 404 Error!")
-    }
-})
+    let changeId;
 
-app.put("/todos/:id", (req,res)=>{
+    if (todoToDel) {
+        for (let i = 0; i < todo.length; i++) {
+            let checkTodo = todo[i];
+            if (checkTodo.id == wantedId) {
+                todo.splice(i, 1);
+                changeId = i;
+            }
+        }
+        
+        for(let i = changeId; i < todo.length; i++) {
+            todo[i].id--
+        }
+
+        fs.writeFileSync("todos.json", JSON.stringify(todo));
+        todo = JSON.parse(fs.readFileSync("todos.json", "utf8"));
+        res.status(204).send("todo deleted!");
+    } else {
+        res.status(404).send("Todo Not Found To Delete! 404 Error!");
+    }
+});
+
+app.put("/todos/:id", (req, res) => {
     const wantedId = parseInt(req.params.id);
     let showErr = true;
-    
-    todo.forEach((task)=>{
-        if (task.id == wantedId){
+
+    todo.forEach(task => {
+        if (task.id == wantedId) {
             showErr = false;
-            
-            if(task.done == false) {
-                task.done = true
+
+            if (task.done == false) {
+                task.done = true;
             } else {
-                task.done = false
+                task.done = false;
             }
-            
+
             res.json(task);
         }
-    })
-    
-    if(showErr) {
-        res.status(404).send("Task Not Found! 404 Error!")
-    }
-})
+    });
+    fs.writeFileSync("todos.json", JSON.stringify(todo));
+    todo = JSON.parse(fs.readFileSync("todos.json", "utf8"));
 
-app.listen(port, ()=>{
+    if (showErr) {
+        res.status(404).send("Task Not Found! 404 Error!");
+    }
+});
+
+app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-})
+});
